84a09257d35e3453fb0e55044ccbb106df5019b1
Matiboux <matiboux@gmail.com>
2026-07-15 01:27:01 +0200

    Update create-user.sh to add user to sudoers

--- src/common/create-user.sh
+++ src/common/create-user.sh
@@ -4,6 +4,13 @@ set -e
 USER=${1:-user}
 UID=${2:-1000}
 GID=${3:-1000}
+SUDOER=${4:-true}
 
+# Create group and user
 groupadd -g ${GID} ${USER}
 useradd -lm -u ${UID} -g ${GID} ${USER}
+
+# Add user to sudoers (if sudo is installed)
+if [ "${SUDOER}" = 'true' ] && command -v sudo > /dev/null 2>&1; then
+	usermod -aG sudo ${USER}
+fi
