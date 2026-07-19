f75f023263e3747bab48e2622f084b9a382141bb
Matiboux <matiboux@gmail.com>
2026-07-20 01:55:48 +0200

    Update create-user.sh script

--- src/common/create-user.sh
+++ src/common/create-user.sh
@@ -1,16 +1,32 @@
 #!/bin/sh
 set -e
 
-USER=${1:-user}
-UID=${2:-1000}
-GID=${3:-1000}
-SUDOER=${4:-true}
+USERNAME="$1"
+USER_ID="$2"
+GROUP_ID="$3"
+SUDO_USER="$4"
+
+if [ -z "${USERNAME}" ]; then
+	USERNAME='user'
+fi
+
+if [ -z "${USER_ID}" ]; then
+	USER_ID='1000'
+fi
+
+if [ -z "${GROUP_ID}" ]; then
+	GROUP_ID='1000'
+fi
+
+if [ -z "${SUDO_USER}" ]; then
+	SUDO_USER='false'
+fi
 
 # Create group and user
-groupadd -g ${GID} ${USER}
-useradd -lm -u ${UID} -g ${GID} ${USER}
+groupadd -g "${GROUP_ID}" "${USERNAME}"
+useradd -lm -u "${USER_ID}" -g "${GROUP_ID}" "${USERNAME}"
 
 # Add user to sudoers (if sudo is installed)
-if [ "${SUDOER}" = 'true' ] && command -v sudo > /dev/null 2>&1; then
-	usermod -aG sudo ${USER}
+if [ "${SUDO_USER}" = 'true' ] && command -v sudo > /dev/null 2>&1; then
+	usermod -aG sudo "${USERNAME}"
 fi
